18436987ea552bd0753db839cb13870f318025fb
Matt Tarbit <mtarbit@gmail.com>
2013-02-01 23:25:09 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-keys
+Keys
 ====
 
 Easy keyboard-handling for jQuery
+
+### Example usage
+
+The basic handler pattern is a switch statement with cases catching named 
+keys or key combinations. Returning true on a successful match to prevent
+further event propogation. Or false to allow the event to continue on to 
+any other handlers.
+
+  keys.addHandler(function(key){
+    switch (key) {
+      case 'space':
+        // Do something
+        break;
+
+      case 'enter':
+        // Do something else
+        break;
+
+      default:
+        return false;
+        break;
+    }
+
+    return true;
+  });
+
+The switch statement lets you map multiple keys or combos to a single action:
+
+  keys.addHandler(function(key){
+    switch (key) {
+      case 'shift-tab':
+      case 'left':
+      case 'h':
+        // Go left
+        break;
+
+      case 'up':
+      case 'k':
+        // Go up
+        break;
+
+      case 'tab':
+      case 'right':
+      case 'l':
+        // Go right
+        break;
+
+      case 'down':
+      case 'j':
+        // Go down
+        break;
+
+      case 'space':
+      case 'enter':
+        // Perform an action
+        break;
+
+      default:
+        return false;
+        break;
+    }
+
+
---
+++ keys.js
@@ -0,0 +1,65 @@
+// For specifics of key code support see:
+// http://www.quirksmode.org/js/keys.html
+
+var keys = {};
+
+keys.lookup = {
+      8: 'backspace'
+  ,   9: 'tab'
+  ,  13: 'enter'
+  ,  27: 'esc'
+  ,  32: 'space'
+  ,  33: 'page-up'
+  ,  34: 'page-down'
+  ,  35: 'end'
+  ,  36: 'home'
+  ,  37: 'left'
+  ,  39: 'right'
+  ,  38: 'up'
+  ,  40: 'down'
+  ,  46: 'delete'
+};
+
+keys.name = function(e){
+  var code = e.keyCode;
+
+  var isAtoZ = code >= 65 && code <= 90;
+  var is0to9 = code >= 48 && code <= 57;
+  var isMeta = code == 91 || code == 93 || (code >= 16 && code <= 18);
+
+  var char, meta = [];
+
+  if (e.metaKey) meta.push('cmd');
+  if (e.ctrlKey) meta.push('ctrl');
+  if (e.altKey)  meta.push('alt');
+
+  if (e.shiftKey) {
+    if (isAtoZ) {
+      char = String.fromCharCode(code);
+    } else {
+      meta.push('shift');
+    }
+  } else if (isAtoZ) {
+    char = String.fromCharCode(code + 32);
+  } else if (is0to9) {
+    char = '' + (code - 48);
+  }
+
+  if (!char && !isMeta) {
+    char = this.lookup[code] || '[' + code + ']';
+  }
+
+  if (meta.length) {
+    if (char) meta.push(char);
+    char = meta.join('-');
+  }
+
+  return char;
+};
+
+keys.addHandler = function(handler){
+  $(document).keydown(function(e){
+    var caught = handler(result);
+    if (caught) e.stopImmediatePropagation();
+  });
+};
